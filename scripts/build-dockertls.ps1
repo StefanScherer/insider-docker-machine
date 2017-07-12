bacd520805d0f72460dba8469e7b1af533e5b548
Stefan Scherer <scherer_stefan@icloud.com>
2017-07-12 08:39:20 +0200

    build dockertls and tag it

--- scripts/build-dockertls.ps1
+++ scripts/build-dockertls.ps1
@@ -3,5 +3,9 @@ cd C:\Users\stefan\code\insider\dockertls
 Write-Host Building dockertls image
 docker build -t dockertls .
 
-Write-Host Testing dockertls image
-docker run --rm -e SERVER_NAME=test -e IP_ADDRESSES=127.0.0.1 dockertls
+docker images
+
+# Write-Host Testing dockertls image
+# docker run --rm -e SERVER_NAME=test -e IP_ADDRESSES=127.0.0.1 dockertls
+
+docker tag dockertls stefanscherer/dockertls-windows:insider
