4ba77bec2f66c52a0a7bb56c87cb6cd1b595b644
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-07 16:11:52 +0200

    Update to Docker 17.09.0-ce-rc1

--- scripts/load-images.ps1
+++
@@ -1,4 +0,0 @@
-cd C:\Users\stefan\code\insider
-Start-Sleep 30
-docker import CBaseOs_rs_prerelease_16237.1001.170701-0549_amd64fre_NanoServer_en-us.tar.gz microsoft/nanoserver
-docker import CBaseOs_rs_prerelease_16237.1001.170701-0549_amd64fre_ServerDatacenterCore_en-us.tar.gz microsoft/windowsservercore
