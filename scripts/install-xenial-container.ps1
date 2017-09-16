cdd776cb88282fd9888c484e7852eacbac0ebf5c
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-16 12:36:07 +0200

    Fix temp filename

--- scripts/install-xenial-container.ps1
+++ scripts/install-xenial-container.ps1
@@ -1,27 +1,18 @@
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-$Root = "$Env:ProgramFiles\Linux Containers"
-if (!(Test-Path $Root)) {
-  mkdir $Root
-  # Give the virtual machines group full control
-  $acl = Get-Acl -Path $Root
-  $vmGroupRule = new-object System.Security.AccessControl.FileSystemAccessRule("NT VIRTUAL MACHINE\Virtual Machines", "FullControl","ContainerInherit,ObjectInherit", "None", "Allow")
-  $acl.SetAccessRule($vmGroupRule)
-  Set-Acl -AclObject $acl -Path $Root
-}
-
 iwr -useb https://chocolatey.org/install.ps1 | iex
 choco install -y 7zip
 
 Write-Host Downloading xenial-container
-Invoke-WebRequest -UseBasicParsing -OutFile $Env:Temp\container.xz https://partner-images.canonical.com/hyper-v/linux-containers/xenial/current/xenial-container-hyperv.vhdx.xz
+$Root = "$Env:ProgramFiles\Linux Containers"
+Invoke-WebRequest -UseBasicParsing -OutFile $Env:Temp\container.vhdx.xz https://partner-images.canonical.com/hyper-v/linux-containers/xenial/current/xenial-container-hyperv.vhdx.xz
 
 7z x $env:TEMP\container.vhdx.xz -o"$Root"
 
 mv "$Root\container.vhdx" "$Root\uvm.vhdx"
 
-rm $Env:Temp\container.xz
+rm $Env:Temp\container.vhdx.xz
 
 Write-Host "Activating experimental features"
 $daemonJson = "$env:ProgramData\docker\config\daemon.json"
