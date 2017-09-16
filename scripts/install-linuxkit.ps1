c62875c128aff464d1c548cfdc032832d6a3dc11
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-16 12:35:38 +0200

    Cleanup

--- scripts/install-linuxkit.ps1
+++ scripts/install-linuxkit.ps1
@@ -2,15 +2,6 @@ $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
 $Root = "$Env:ProgramFiles\Linux Containers"
-if (!(Test-Path $Root)) {
-  mkdir $Root
-  # Give the virtual machines group full control
-  $acl = Get-Acl -Path $Root
-  $vmGroupRule = new-object System.Security.AccessControl.FileSystemAccessRule("NT VIRTUAL MACHINE\Virtual Machines", "FullControl","ContainerInherit,ObjectInherit", "None", "Allow")
-  $acl.SetAccessRule($vmGroupRule)
-  Set-Acl -AclObject $acl -Path $Root
-}
-
 Invoke-WebRequest -UseBasicParsing -OutFile $Env:Temp\linuxkit.zip https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip
 
 Expand-Archive $Env:Temp\linuxkit.zip -DestinationPath "$Env:ProgramFiles\Linux Containers"
