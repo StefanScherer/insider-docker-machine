412b679fb8b88a6d58948bd90e03b041ceca3b84
Stefan Scherer <scherer_stefan@icloud.com>
2017-09-16 11:18:38 +0200

    Set experimental

--- scripts/install-linuxkit.ps1
+++ scripts/install-linuxkit.ps1
@@ -1,12 +1,29 @@
 $ErrorActionPreference = 'Stop'
 $ProgressPreference = 'SilentlyContinue'
 
-mkdir "$Env:ProgramFiles\Linux Containers"
+$Root = "$Env:ProgramFiles\Linux Containers"
+if (!(Test-Path $Root)) {
+  mkdir $Root
+  # Give the virtual machines group full control
+  $acl = Get-Acl -Path $Root
+  $vmGroupRule = new-object System.Security.AccessControl.FileSystemAccessRule("NT VIRTUAL MACHINE\Virtual Machines", "FullControl","ContainerInherit,ObjectInherit", "None", "Allow")
+  $acl.SetAccessRule($vmGroupRule)
+  Set-Acl -AclObject $acl -Path $Root
+}
 
 Invoke-WebRequest -UseBasicParsing -OutFile $Env:Temp\linuxkit.zip https://github.com/friism/linuxkit/releases/download/preview-1/linuxkit.zip
 
 Expand-Archive $Env:Temp\linuxkit.zip -DestinationPath "$Env:ProgramFiles\Linux Containers"
 rm $Env:Temp\linuxkit.zip
+
+Write-Host "Activating experimental features"
+$daemonJson = "$env:ProgramData\docker\config\daemon.json"
+$config = @{}
+if (Test-Path $daemonJson) {
+  $config = (Get-Content $daemonJson) -join "`n" | ConvertFrom-Json
+}
+$config = $config | Add-Member(@{ experimental = $true }) -Force -PassThru
+$config | ConvertTo-Json | Set-Content $daemonJson -Encoding Ascii
 
 [Environment]::SetEnvironmentVariable("LCOW_SUPPORTED", "1", "Machine")
 
